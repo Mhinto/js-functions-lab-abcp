40caada131a91c1edfbf7eb4e4f56ba9c34d644c
Michael Hinton <michael01hinton@gmail.com>
2018-02-23 01:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code below!
+function happyHolidays(string) {
+  return 'Happy holidays!'
+}
+function happyHolidaysTo(name) {
+  console.log("Happy holidays, ${name}!")
+}
+function happyHolidayTo(holiday,name) {
+ console.log("Happy ${holiday}. ${name}!")
+}
